53f2be14f9b864bba6e35fc5e574ad9cc13d4c3a
Zhenua Kovalev <111678905+Fearester@users.noreply.github.com>
2022-11-26 22:27:39 +0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -74,7 +74,7 @@ class Main extends Sprite
 		var stageWidth:Int = Lib.current.stage.stageWidth;
 		var stageHeight:Int = Lib.current.stage.stageHeight;
 
-		if (zoom == -1 && flixel < 5.0.0)
+		if (zoom == -1)
 		{
 			var ratioX:Float = stageWidth / gameWidth;
 			var ratioY:Float = stageHeight / gameHeight;
