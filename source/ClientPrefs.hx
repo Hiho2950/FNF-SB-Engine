45512a585cb5661e33def60d18f20ed8d3ba5582
Stefan[BETA]™ <stefanbeta2008@gmail.com>
2022-12-09 15:25:15 +0100

    UGH, What I Have Done

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -10,7 +10,7 @@ class ClientPrefs {
 	public static var downScroll:Bool = false;
 	public static var middleScroll:Bool = false;
 	public static var opponentStrums:Bool = true;
-	public static var showFPS:Bool = #if android false #else true #end;
+	public static var showFPS:Bool = #if android true #else true #end;
 	public static var flashing:Bool = true;
 	public static var globalAntialiasing:Bool = true;
 	public static var noteSplashes:Bool = true;
@@ -22,7 +22,7 @@ class ClientPrefs {
 	public static var camZooms:Bool = true;
 	public static var hideHud:Bool = false;
 	public static var hideWatermark:Bool = false;
-	public static var judgementCounter:Bool = false;
+	public static var judgementCounter:Bool = true;
 	public static var noteOffset:Int = 0;
 	public static var arrowHSV:Array<Array<Int>> = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]];
 	public static var ghostTapping:Bool = true;
