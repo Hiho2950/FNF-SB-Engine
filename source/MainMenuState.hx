996cef3e51360ea061430b5e3e582b406490d825
Stefan[BETA]™ <stefanbeta2008@gmail.com>
2022-12-19 01:54:31 +0100

    Please Work. Im Don't Angry If Haxelib Give Again

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -47,7 +47,7 @@ class MainMenuState extends MusicBeatState
 	var magenta:FlxSprite;
 	var camFollow:FlxObject;
 	var camFollowPos:FlxObject;
-	var checker = new FlxBackdrop(Paths.image('Main_Checker'), XY, 0.2, 0.2);
+	var checker = new FlxBackdrop(Paths.image('Main_Checker');
 	var gradientBar:FlxSprite = new FlxSprite(0, 0).makeGraphic(FlxG.width, 300, 0xFFFFA500);
 	var debugKeys:Array<FlxKey>;
 
