7beb0953242ddcddbf54040173c0c47a424a6d2d
Stefan[BETA]™ <stefanbeta2008@gmail.com>
2023-01-05 07:12:11 +0100

    Some small changes for PC issues i think

--- source/Main.hx
+++ source/Main.hx
@@ -44,6 +44,10 @@ class Main extends Sprite
 	{
 		Lib.current.addChild(new Main());
 	}
+	
+	#if (flixel < "5.0.0")
+	@:deprecated("Please update to the latest version of HaxeFlixel.")
+	#end
 
 	public function new()
 	{
@@ -86,7 +90,7 @@ class Main extends Sprite
 		SUtil.check();
 	
 		ClientPrefs.loadDefaultKeys();
-		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
+		addChild(new FlxGame(gameWidth, gameHeight, initialState, #if (flixel < "5.0.0") zoom, #end framerate, framerate, skipSplash, startFullscreen));
 
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
