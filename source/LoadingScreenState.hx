59c2a6a1fc8a32341372559bf8cbc7f7b6513441
Zhenua Kovalev <111678905+Fearester@users.noreply.github.com>
2023-01-14 17:48:43 +0300

    Goodluck...

--- source/LoadingScreenState.hx
+++ source/LoadingScreenState.hx
@@ -1,46 +1,113 @@
+#if sys
 package;
-
-import flixel.FlxState;
 import flixel.FlxG;
 import flixel.FlxSprite;
+import flixel.addons.transition.FlxTransitionSprite.GraphicTransTileDiamond;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.addons.transition.TransitionData;
+import flixel.graphics.FlxGraphic;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.math.FlxPoint;
+import flixel.util.FlxTimer;
+import flixel.text.FlxText;
+import flixel.system.FlxSound;
+import lime.app.Application;
+#if desktop
+import Discord.DiscordClient;
+#end
+import openfl.display.BitmapData;
+import openfl.utils.Assets;
+import haxe.Exception;
+import flixel.tweens.FlxEase;
+import flixel.util.FlxColor;
+import flixel.tweens.FlxTween;
+#if cpp
+import sys.FileSystem;
+import sys.io.File;
+#end
+
+using StringTools;
 
 class LoadingScreenState extends MusicBeatState
 {
-    var progress:Float;
-    var nextScene:String;
-    var loadingTime:Float;
-    var timeElapsed:Float;
-    var loadingBar:FlxSprite;
+	public static var bitmapData:Map<String,FlxGraphic>;
+	public static var bitmapData2:Map<String,FlxGraphic>;
 
-    public function new()
-    {
-        super();
-        progress = 0.0;
-        loadingTime = 10;
-        timeElapsed = 0;
-        nextScene = "MainScene";
-        loadingBar = new FlxSprite('timeBar');
-        loadingBar.makeGraphic(100, 20, 0x00FFA500);
-        add(loadingBar);
-        // Start loading the next scene
-        FlxG.loadLibrary(nextScene, onComplete);
-    }
+	var images = [];
+	var music = [];
 
-    private function onComplete():Void{
-        // Update the loading bar or animation
-        progress = FlxG.library.getProgress();
-        // Once the next scene is fully loaded, switch to it
-        if(progress == 1.0){
-            MusicBeatState.switchState(new TitleState());
-        }
-    }
+	var shitz:FlxText;
 
-    override public function update():Void{
-        super.update();
-        timeElapsed += FlxG.elapsed;
-        if (timeElapsed >= loadingTime)
-        {
-            MusicBeatState.switchState(new TitleState());
-        }
-    }
+	override function create()
+	{
+		FlxG.mouse.visible = false;
+
+		FlxG.worldBounds.set(0,0);
+
+		bitmapData = new Map<String,FlxGraphic>();
+		bitmapData2 = new Map<String,FlxGraphic>();
+
+		var menuBG:FlxSprite = new FlxSprite().loadGraphic(Paths.image('funkay'));
+		menuBG.screenCenter();
+		add(menuBG);
+
+		shitz = new FlxText(12, 630, 300, "Loading...", 12);
+		shitz.scrollFactor.set();
+		shitz.setFormat("VCR OSD Mono", 50, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		add(shitz);
+
+		#if sys
+		for (i in FileSystem.readDirectory(FileSystem.absolutePath("assets/shared/images/characters")))
+		{
+			if (!i.endsWith(".png"))
+				continue;
+			images.push(i);
+		}
+
+		for (i in FileSystem.readDirectory(Paths.inst(PlayState.SONG.song)))
+		{
+			music.push(i);
+		}
+		#end
+
+		sys.thread.Thread.create(() -> {
+			cache();
+		});
+
+		super.create();
+	}
+
+	override function update(elapsed) 
+	{
+		super.update(elapsed);
+	}
+
+	function cache()
+	{
+		#if !linux
+
+		for (i in images)
+		{
+			var replaced = i.replace(".png","");
+			var data:BitmapData = BitmapData.fromFile("assets/shared/images/characters/" + i);
+			var graph = FlxGraphic.fromBitmapData(data);
+			graph.persist = true;
+			graph.destroyOnNoUse = false;
+			bitmapData.set(replaced,graph);
+			trace(i);
+		}
+
+
+
+		for (i in music)
+		{
+			trace(i);
+		}
+
+
+		#end
+		FlxG.switchState(new TitleState());
+	}
+
 }
+#end
